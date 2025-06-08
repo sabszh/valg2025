37290b69ab7a57ea31056a4bb45862d30e61138e
sabszh <87586469+sabszh@users.noreply.github.com>
2025-06-08 15:14:01 +0200

    Update kandidatur.py

--- kandidatur.py
+++ kandidatur.py
@@ -4,7 +4,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # --- Indstillinger ---
-candidates = ["Anna", "Bjørn", "Clara", "David", "Elias"]
+candidates = ["Test1", "Test2", "Test3", "Test4", "Test5"]
 sheet_name = "Bestyrelsesvalg 2025"
 
 # --- Funktion: Generér anonym ID ---
